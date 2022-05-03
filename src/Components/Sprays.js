4ff313d0469a006903ef6d37adac92c6bfa87462
Aaron <aaronlo826@gmail.com>
2022-05-03 17:13:38 -0400

    created button that generates random number, grabs object using that number and displays spray

--- src/Components/Sprays.js
+++ src/Components/Sprays.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const Sprays = () => {
   const [spray, setSpray] = useState([]);
+  const [randomSpray, setRandomSpray] = useState({});
 
   useEffect(() => {
     axios({
@@ -10,15 +11,14 @@ const Sprays = () => {
       method: "GET",
       dataRespone: "json",
     }).then((response) => {
-      // console.log(response)
       const sprayArray = response.data.data;
 
       const finalData = sprayArray.map((sprayData, index) => {
         const sprayName = sprayData.displayName;
         const sprayImg = sprayData.fullTransparentIcon;
         const sprayGif = sprayData.animationPng;
-        const sprayId = {index};
-        const sprayKey = sprayData.uuid;
+        const sprayId = sprayData.uuid;
+        const sprayKey = {index};
         let sprayDisplay = "";
 
         if (sprayGif === null) {
@@ -34,13 +34,37 @@ const Sprays = () => {
           key:sprayKey
         }
       });
-      console.log(finalData)
       setSpray(finalData);
+      // console.log(finalData)
     });
   }, []);
 
+  // const [singleSpray, setSingleSpray] = useState([spray]);
+  // useEffect(() => {
+  //   console.log(spray)
+  // })
+
+  const handleRandomSpray = () => {
+    const index = Math.floor(Math.random() * spray.length +1)
+    const randomSprayItem = spray[index]
+
+    setRandomSpray(randomSprayItem);
+  }
+
+
   return (
     <section id="sprays">
+      <button onClick={() => {handleRandomSpray()}}>Test</button>
+      {
+        randomSpray.id !== undefined ? (
+          <div>
+            <img src={randomSpray.display} alt={randomSpray.name} />
+            <p>{randomSpray.name}</p>
+          </div>
+        ): null
+
+      }
+
 
     </section>
   );
