32e19a8080cb4202c8352f1f999145a60b8bdf50
Aaron <aaronlo826@gmail.com>
2022-05-03 11:00:48 -0400

    styled contact component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,12 @@
 const About = () => {
   return (
-    <div id="about">
+    <section id="about">
       <div className="wrapper">
         <h2>About</h2>
         <h3>Valorant Spray</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia aut quae eum quasi deleniti ex iure praesentium veritatis labore harum. Sit, sed ex tenetur aut aliquam magni repellendus beatae neque vitae facere quisquam! Beatae tempora ullam deleniti nam quasi eum soluta, ducimus natus ea quas. Quam praesentium officia enim repellat.</p>
       </div>
-    </div>
+    </section>
 
   );
 };
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,37 +4,27 @@ import { brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const Contact = () => {
   return (
-    <div id="contact">
+    <section id="contact">
       <div className="wrapper">
-        <section className="contact" id="contact">
-          <div className="wrapper">
-            <div className="socialIcons">
-              <a href="#">
-                <FontAwesomeIcon icon={brands("twitter")} />
-                <span>Link to Twitter</span>
-              </a>
-              <a href="#">
-                <FontAwesomeIcon icon={brands("github")} />
-                <span>Link to Pinterest</span>
-              </a>
-              <a href="#">
-                <FontAwesomeIcon icon={brands("Linkedin")} />
-                <span>Link to LinkedIn</span>
-              </a>
-            </div>
-            <div className="contactEmail">
-              <h4>sprays@valorant.com</h4>
-            </div>
-            <div className="contactInfo">
-              <p>416-825-7268(416-VALRANT)</p>
-              <p>132 Terrybay Rd</p>
-              <p>Toronto, ON</p>
-              <p>M2Z2T3</p>
-            </div>
-          </div>
-        </section>
+        <div className="socialIcons">
+          <a href="#">
+            <FontAwesomeIcon className="icons" icon={brands("github")} size="2x" />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon className="icons"  icon={brands("Linkedin")} size="2x" />
+          </a>
+        </div>
+        <div className="contactEmail">
+          <p>sprays@valorant.com</p>
+        </div>
+        <div className="contactInfo">
+          <p>416-825-7268(416-VALRANT)</p>
+          <p>132 Terrybay Rd</p>
+          <p>Toronto, ON</p>
+          <p>M2Z2T3</p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
